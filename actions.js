fee5dffc7764dfb7f2689ba5363d151f4b992334
Sandroisu <sandroisu1411@gmail.com>
2022-04-18 22:18:58 +0300

    done home

--- actions.js
+++ actions.js
@@ -1,16 +1,13 @@
 function hideSideMenu() {
-    var x = document.getElementById("sideMenu");
-    if (x.style.display === "none") {
-        return;
-    } else {
-        x.style.display = "none";
-    }
+    const x = document.getElementById("sideMenu");
+
+        x.classList.remove("opened");
+        x.classList.add("closed")
+
 }
 
 function showSideMenu() {
-    var x = document.getElementById("sideMenu");
-    var y = document.getElementById("mainContainer");
-    if (y.offsetWidth > 1600) {
-        x.style.display = "block"
-    }
+    const x = document.getElementById("sideMenu");
+    x.classList.remove("closed");
+    x.classList.add("opened")
 }
